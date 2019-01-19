3d93b7d42f19b7729f367450ea7bb26b46f7207b
James Newton-King <james@newtonking.com>
2019-01-19 19:30:12 +1300

    Add microbenchmarks project

---
+++ src/Grpc.AspNetCore.Server/IMessageParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grpc.AspNetCore
+{
+    public interface IMessageParser
+    {
+        // TODO: Read message without allocating array
+        object ParseFrom(byte[] data);
+    }
+}
